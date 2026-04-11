683743cfe8c421af9f3d5251572cac99c0aefe50
Warcoor <daniilmax09@gmail.com>
2026-04-11 17:30:01 +0300

    Change default port from 3000 to 8080

--- wbs.py
+++ wbs.py
@@ -35,7 +35,7 @@ def handle_message(msg):
     # отправляем всем
     send(msg, broadcast=True, include_self=False)
 
-port = int(os.environ.get("PORT", 3000))
+port = int(os.environ.get("PORT", 8080))
 if __name__ == "__main__":
     init_db()
     socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
