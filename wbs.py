a6a2bdf967f7dc53a9e4ff6fa3635aab2f85fde7
Warcoor <daniilmax09@gmail.com>
2026-04-11 18:12:03 +0300

    Refactor to use render_template and add DB support
    Refactor HTML rendering and add database integration.

--- wbs.py
+++ wbs.py
@@ -1,60 +1,41 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 from flask_socketio import SocketIO, send
+import psycopg2
 import os
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# простой HTML прямо в коде (чтобы не зависеть от templates)
-HTML = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Test Chat</title>
-</head>
-<body>
-    <h2>SocketIO Test</h2>
-    <input id="msg" placeholder="message">
-    <button onclick="sendMsg()">Send</button>
 
-    <ul id="chat"></ul>
+DATABASE_URL = "postgresql://postgres:ARHmeHekAknZBGekTCctDKhqzENFdnZY@metro.proxy.rlwy.net:29944/railway"
+conn = None
+cursor = None
 
-    <script src="https://cdn.socket.io/4.7.5/socket.io.min.js"></script>
-    <script>
-        const socket = io();
-
-        socket.on("message", (msg) => {
-            const li = document.createElement("li");
-            li.innerText = msg;
-            document.getElementById("chat").appendChild(li);
-        });
-
-        function sendMsg() {
-            const msg = document.getElementById("msg").value;
-            socket.send(msg);
-        }
-    </script>
-</body>
-</html>
-"""
+def init_db():
+    global conn, cursor
+    conn = psycopg2.connect(DATABASE_URL, sslmode="require")
+    cursor = conn.cursor()
 
 @app.route("/")
 def index():
-    return render_template_string(HTML)
+    return render_template("index.html")
 
 
 @socketio.on("message")
 def handle_message(msg):
-    print("Received:", msg)
+    print("Получено:", msg)
+
+    # сохраняем в БД
+    cursor.execute(
+        "INSERT INTO Messages (TextMessage) VALUES (%s)",
+        (msg,)
+    )
+    conn.commit()
+
+    # отправляем всем
     send(msg, broadcast=True, include_self=False)
 
-
+port = int(os.environ.get("PORT"))
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-
-    socketio.run(
-        app,
-        host="0.0.0.0",
-        port=port,
-        allow_unsafe_werkzeug=True
-    )
+    init_db()
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
